f0d10426e62fdcb348ee2cb49d360c286ca57179
bpnagle <145621344+bpnagle@users.noreply.github.com>
2024-01-28 20:25:02 -0500

    Add files via upload

---
+++ crypto.sql
@@ -0,0 +1,17 @@
+Create table bitcoin1(
+	Date DATE,
+	Open Float,
+	High Float,
+	Low Float,
+	Close Float);
+	
+Select * from bitcoin1
+
+Create table ethereum1(
+	date Date,
+	Open Float,
+	High Float,
+	Low Float,
+	Close Float);
+	
+	Select * from ethereum1
