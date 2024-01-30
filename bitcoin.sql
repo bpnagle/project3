fb16c70d5cb23fb60e06bf04e042344bcf0ad031
bpnagle <145621344+bpnagle@users.noreply.github.com>
2024-01-29 21:18:52 -0500

    Add files via upload

---
+++ bitcoin.sql
@@ -0,0 +1,19 @@
+Create table bitcoin(
+	Date date,
+	Open float,
+	High float,
+	Low float,
+	Close float,
+	difference float,
+	percentage_difference float);
+	Select * from bitcoin
+	
+Create table ethereum(
+	Date date,
+	Open float,
+	High float,
+	Low float,
+	Close float,
+	difference float,
+	percentage_difference float);
+	Select * from ethereum
