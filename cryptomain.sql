84c3ff58f29454b90960259345342b7e635e7905
bpnagle <145621344+bpnagle@users.noreply.github.com>
2024-02-01 20:07:25 -0500

    Add files via upload

---
+++ cryptomain.sql
@@ -0,0 +1,29 @@
+Create table bitcoin(
+	Date date Not Null,
+	Open float Not Null,
+	High float Not Null,
+	Low float Not Null,
+	Close float Not Null,
+	Percent_Change float not Null);
+
+	Select * from bitcoin
+	
+	Create table ethereum(
+	Date date Not Null,
+	Open float Not Null,
+	High float Not Null,
+	Low float Not Null,
+	Close float Not Null,
+	Percent_Change float Not Null);
+
+	Select * from ethereum
+	
+	Create table doge(
+	Date date Not Null,
+	Open float Not Null,
+	High float Not Null,
+	Low float Not Null,
+    Close float Not Null,
+	Percent_Change float Not Null);
+	Select * from doge
+	
